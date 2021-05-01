c8865131349a6cba0a4cf27c61bac608a32f40d2
Julio <juliocesardiniznogueira@gmail.com>
2021-05-01 16:22:19 -0300

    Adding responsivity

--- src/components/Main.js
+++ src/components/Main.js
@@ -21,40 +21,40 @@ const Main = () => {
     <>
       {userData.map((item) => (
         <div className="Card" key={item.login.uuid}>
-          <div className="Image">
+          <div className="Image Flex-item">
             <img src={item.picture.large} alt="foto de perfil" />
           </div>
 
-          <div>
+          <div className="Flex-item">
             <h2>Nome</h2>
             <p>{`${item.name.first} ${item.name.last}`}</p>
           </div>
 
-          <div>
+          <div className="Flex-item">
             <h2>Localização</h2>
             <p>{`${item.location.city} - ${item.location.state}, ${item.location.country}`}</p>
           </div>
 
-          <div>
+          <div className="Flex-item">
             <h2>Idade</h2>
             <p>{item.dob.age} Anos</p>
           </div>
 
-          <div>
+          <div className="Flex-item">
             <h2>E-mail</h2>
             <p> {item.email}</p>
           </div>
 
-          <div>
+          <div className="Flex-item">
             <h2>Gênero</h2>
             <p>{item.gender === "female" ? "Female" : "Male"}</p>
           </div>
 
-          <div className="Button">
+          <div className="Button Flex-item">
             <button onClick={getUser}>Gerar Nova Pessoa</button>
           </div>
 
-          <div>
+          <div className="Flex-item">
             <h2>Telefone</h2>
             <p>{item.cell}</p>
           </div>
